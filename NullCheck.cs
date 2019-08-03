ae684a49715725678ab7f115e69de2f6d9d75ea8
jayamohanan <37545914+jayamohanan@users.noreply.github.com>
2019-08-03 23:13:56 +0530

    Add files via upload

---
+++ NullCheck.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NullCheck  {
+
+    public static void IsNull(params Object[] objects)
+    {
+        string nullObjNames="";
+        List<Object> nullObjects = new List<Object>();
+        foreach (var obj in objects)
+        {
+            if (obj == null)
+            {
+                nullObjects.Add(obj);
+            }
+        }
+        foreach (var obj in nullObjects)
+        {
+            nullObjNames += obj.name + "  ";
+        }
+        if(nullObjNames == "")
+        {
+            Debug.Log("No null values found");
+            return;
+        }
+        Debug.Log(nullObjNames);
+
+    }
+
+	
+}
